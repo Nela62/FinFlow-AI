1adf771b4b23358e5f26ff720b40b91ea041552c
Helen Kochetkova <helen@finpanel.com>
2024-11-27 20:50:21 -0800

    seed and new migration

---
+++ supabase/migrations/20241127204305_create_workflows_table.sql
@@ -0,0 +1,117 @@
+CREATE TABLE IF NOT EXISTS workflows(
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id uuid NOT NULL REFERENCES auth.users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  name text NOT NULL
+);
+
+ALTER TABLE workflows ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Enable ALL to authenticated users based on user id" ON "workflows" TO "authenticated"
+  USING (((
+    SELECT
+      "auth"."uid"() AS "uid") = "user_id"))
+      WITH CHECK (((
+        SELECT
+          "auth"."uid"() AS "uid") = "user_id"));
+
+CREATE TABLE IF NOT EXISTS runs(
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id uuid NOT NULL REFERENCES auth.users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  workflow_id uuid NOT NULL REFERENCES workflows(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  name text NOT NULL,
+  status text NOT NULL DEFAULT 'PENDING'
+);
+
+ALTER TABLE runs ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Enable ALL to authenticated users based on user id" ON "runs" TO "authenticated"
+  USING (((
+    SELECT
+      "auth"."uid"() AS "uid") = "user_id"))
+      WITH CHECK (((
+        SELECT
+          "auth"."uid"() AS "uid") = "user_id"));
+
+CREATE TABLE IF NOT EXISTS actions(
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id uuid NOT NULL REFERENCES auth.users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  run_id uuid NOT NULL REFERENCES runs(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  action_id uuid NOT NULL REFERENCES actions(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  name text NOT NULL,
+  status text NOT NULL,
+  created_at timestamp with time zone DEFAULT now() NOT NULL,
+  updated_at timestamp with time zone DEFAULT now() NOT NULL
+);
+
+ALTER TABLE actions ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE IF NOT EXISTS subactions(
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id uuid NOT NULL REFERENCES auth.users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  action_id uuid NOT NULL REFERENCES actions(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  name text NOT NULL,
+  status text NOT NULL,
+  created_at timestamp with time zone DEFAULT now() NOT NULL,
+  updated_at timestamp with time zone DEFAULT now() NOT NULL
+);
+
+ALTER TABLE subactions ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Enable ALL to authenticated users based on user id" ON "subactions" TO "authenticated"
+  USING (((
+    SELECT
+      "auth"."uid"() AS "uid") = "user_id"))
+      WITH CHECK (((
+        SELECT
+          "auth"."uid"() AS "uid") = "user_id"));
+
+CREATE POLICY "Enable ALL to authenticated users based on user id" ON "actions" TO "authenticated"
+  USING (((
+    SELECT
+      "auth"."uid"() AS "uid") = "user_id"))
+      WITH CHECK (((
+        SELECT
+          "auth"."uid"() AS "uid") = "user_id"));
+
+CREATE TABLE IF NOT EXISTS nodes(
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id uuid NOT NULL REFERENCES auth.users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  workflow_id uuid NOT NULL REFERENCES workflows(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  name text NOT NULL,
+  type text NOT NULL,
+  position jsonb NOT NULL,
+  data jsonb NOT NULL
+);
+
+ALTER TABLE nodes ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Enable ALL to authenticated users based on user id" ON "nodes" TO "authenticated"
+  USING (((
+    SELECT
+      "auth"."uid"() AS "uid") = "user_id"))
+      WITH CHECK (((
+        SELECT
+          "auth"."uid"() AS "uid") = "user_id"));
+
+CREATE TABLE IF NOT EXISTS edges(
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id uuid NOT NULL REFERENCES auth.users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  workflow_id uuid NOT NULL REFERENCES workflows(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  source uuid NOT NULL REFERENCES nodes(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  target uuid NOT NULL REFERENCES nodes(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  source_handle text NOT NULL,
+  target_handle text NOT NULL,
+  type text NOT NULL,
+  animated boolean NOT NULL
+);
+
+ALTER TABLE edges ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Enable ALL to authenticated users based on user id" ON "edges" TO "authenticated"
+  USING (((
+    SELECT
+      "auth"."uid"() AS "uid") = "user_id"))
+      WITH CHECK (((
+        SELECT
+          "auth"."uid"() AS "uid") = "user_id"));
+
